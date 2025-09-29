44928c9c3f52591eac8e3e88c31e836e2da364b6
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-09-29 13:50:07 +0200

    Ejercicio UD1

---
+++ UD1/PHYTON/Ejercicio7 (par o impar).py
@@ -0,0 +1,5 @@
+print("Introduce un numero:")
+num1 = int(input())
+if num1 % 2 == 0: #Comprobamos si el numero es par o impar con el resto de la division entre 2
+    print("El numero " + str(num1) + " es par")
+else: print("El numero " + str(num1) + " es impar")
