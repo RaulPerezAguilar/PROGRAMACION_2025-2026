a1621733428707b7e87fc56e4cb7a9504590838a
rperagu1310 <rperagu1310@g.educaand.es>
2025-09-29 13:56:56 +0200

    Delete UD1/PHYTON/Ejercicio6 (Año a partir de nacimiento).py

--- UD1/PHYTON/Ejercicio6 (Año a partir de nacimiento).py
+++
@@ -1,5 +0,0 @@
-print("Introduce tu año de nacimiento: ")
-año_nacimiento = int(input())
-año_actual = 2025               # Calculamos la edad restando el año actual menos el año de nacimiento
-edad = año_actual - año_nacimiento
-print("Tienes " + str(edad) + " años.") # Convertimos la edad a cadena para mostrar el resultado
