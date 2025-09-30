e9a14bc7baf0928c373cc0162bcd207a03974275
rperagu1310 <rperagu1310@g.educaand.es>
2025-09-30 13:22:11 +0200

    Delete UD1/PHYTON/Ejercicio6 (mayor o menor).py

--- UD1/PHYTON/Ejercicio6 (mayor o menor).py
+++
@@ -1,8 +0,0 @@
-print("Intruce un numero:")
-num1 = int(input())
-print("Intruce otro numero:")
-num2 = int(input())
-if num1 > num2:    #Comparamos los numeros para saber cual es mayor
-    print("El numero mayor es: " + str(num1))
-    print("El numero menor es: " + str(num2))
-else: print("Los numeros son iguales")
