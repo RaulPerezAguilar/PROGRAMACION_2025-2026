a606f26d6de8c2e9921cfad101f617a4bf0105e7
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-07 10:37:38 +0200

    Ejercicios UD1

---
+++ UD1/PYTHON/Ejercicio14 (Sumar hasta 0).py
@@ -0,0 +1,11 @@
+suma = 0
+n = 0
+while True:
+    print ("Introduce un numero para sumar (0 para salir): ")
+    n = int(input())
+    if n != 0:
+        if n > 0:
+            suma = suma + n
+            print ("Llevas acumulado: ", suma)
+    else: 
+        break
