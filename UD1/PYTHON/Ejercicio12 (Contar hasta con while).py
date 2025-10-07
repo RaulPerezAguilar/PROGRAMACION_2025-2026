5e43da84e35b2811836081883e911889f4450ef1
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-07 09:52:48 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio12 (Contar hasta con while).py
+++ UD1/PYTHON/Ejercicio12 (Contar hasta con while).py
@@ -1,12 +1,15 @@
-i = 0
+i = 1
 while True:
     print ("Hasta que numero quieres contar: ")
     n = int(input())
     if n >= 0:
-        if i == n:
-            break
-        else: 
-            i = i + 1
-            print (i)
+        while n >= i:
+            if n == i:
+                print(i)
+                break
+            else:
+                print(i , end=',')
+                i = i + 1
+        break
     else:
         print ("El numero debe ser mayor que 0")
