6c4b58563090decd6b2af26593c7bf57e744c7d1
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-07 09:05:10 +0200

    Ejercicios UD1

--- UD1/PHYTON/Ejercicio11 (Contar hasta).py
+++ UD1/PHYTON/Ejercicio11 (Contar hasta).py
@@ -1,9 +1,12 @@
-print ("Hasta que numero quieres contar: ")
-usuario = int(input())
 numero = 0
 while True:
-    if numero == usuario:
-        break
-    else: 
-        numero = numero + 1
-        print (numero)
+    print ("Hasta que numero quieres contar: ")
+    usuario = int(input())
+    if usuario >= 0:
+        if numero == usuario:
+            break
+        else: 
+            numero = numero + 1
+            print (numero)
+    else:
+        print ("El numero debe ser mayor que 0")
