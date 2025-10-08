45c8c08b1b75ec2d9165e601e0169096a79e8482
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-08 13:03:53 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio19 (Factorial de un numero).py
+++ UD1/PYTHON/Ejercicio19 (Factorial de un numero).py
@@ -1,5 +1,5 @@
 suma=1
 n=int(input("Introduce un numero para calcular su factorial: "))
-for i in range(n,1,-1): 
+for i in range(n,0,-1): 
     suma*=i 
 print("El factorial de " + str(n) + " es : " + str(suma))
