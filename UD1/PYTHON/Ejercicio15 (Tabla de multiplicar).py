c895e6a3b57c5a6029a3b5e4805bc313ae751bea
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-08 11:19:49 +0200

    Ejercicio UD1

--- UD1/PYTHON/Ejercicio15 (Tabla de multiplicar).py
+++ UD1/PYTHON/Ejercicio15 (Tabla de multiplicar).py
@@ -2,7 +2,8 @@ print("De que numero quieres la tabla de multiplicar (0 para salir): ")
 while True:
     n = int(input())
     if n != 0:
-        for i in range(1,11):
+        m = int(input("Hasta que numero quieres la tabla de multiplicar: "))
+        for i in range(m + 1):
             print(str(n) + " x " + str(i) + " = " + str(n * i))
         print("¿Que tabla de multiplicar quieres ahora? (0 para salir): ")
     else:
