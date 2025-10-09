49fc2bb37c8f1b0262cdb213b56736ba77aba0bf
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-09 09:25:13 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio20 (Par o impar con invocacion).py
+++ UD1/PYTHON/Ejercicio20 (Par o impar con invocacion).py
@@ -1,11 +1,11 @@
 #Creamos un programa que invoque una funcion que determine si un numero es par o impar
-def par_impar(numero): 
-    if numero % 2 == 0:
+def par_impar(n): 
+    if n % 2 == 0:
         return "El numero es par"
     else:
         return "El numero es impar"
 #Solicitamos al usuario que ingrese un numero
-numero = int(input("Ingrese un numero: "))
+n = int(input("Ingrese un numero: "))
 #Invocamos la funcion y mostramos el resultado
-resultado = par_impar(numero)
+resultado = par_impar(n)
 print(resultado)
