dff7200678b242006b8e863b6c134f5cabcea1d8
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-14 13:26:32 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio22 (Redondeo).py
+++ UD1/PYTHON/Ejercicio22 (Redondeo).py
@@ -3,3 +3,6 @@ if n < 0:
     n = abs(n) # Convertimos a positivo si es negativo con abs() que devuelve el valor absoluto de un numero
     redondeado = round(n)
     print(f"El número {n} redondeado es {redondeado}")
+else:
+    redondeado = round(n)
+    print(f"El número {n} redondeado es {redondeado}")
