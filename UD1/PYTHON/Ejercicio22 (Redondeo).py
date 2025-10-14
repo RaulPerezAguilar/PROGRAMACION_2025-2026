d13521b0e6786861f8461ca93ceb5f5ecfcf15e0
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-14 13:33:29 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio22 (Redondeo).py
+++ UD1/PYTHON/Ejercicio22 (Redondeo).py
@@ -1,3 +1,6 @@
 n = float(input("Introduce un número decimal: "))
-n = round(abs(n))
+n = round(abs(float(input(n))))
 print(f"El número redondeado es {n}")
+
+
+#Forma corta: print(round(abs(float(input("Introduce un número decimal: ")))))
