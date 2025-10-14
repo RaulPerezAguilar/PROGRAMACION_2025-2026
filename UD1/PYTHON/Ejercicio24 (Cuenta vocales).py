fcda7368b3fdba4914ec0e0e58f35c52de746e98
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-14 13:40:05 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio24 (Cuenta vocales).py
+++ UD1/PYTHON/Ejercicio24 (Cuenta vocales).py
@@ -1,15 +1,15 @@
 n = str(input("Introduce una frase: "))
 vocales = "aeiouAEIOU"
 contador = 0
-for char in n: 
-    if char in vocales:
+for x in n: 
+    if x in vocales:
         contador += 1
 print(f"La frase tiene {contador} vocales.")
 #si es vocal cambiarla por x e ipmprimir la nueva frase
 nueva_frase = ""
-for char in n:
-    if char in vocales:
+for x in n:
+    if x in vocales:
         nueva_frase += "x"
     else:
-        nueva_frase += char
+        nueva_frase += x
 print("La nueva frase es:", nueva_frase)
