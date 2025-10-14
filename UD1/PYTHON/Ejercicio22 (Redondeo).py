9fbc606671e4972fd512c7888fa7165ad8206b99
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-14 13:19:57 +0200

    Ejercicios UD1

--- UD1/PYTHON/Ejercicio22 (Redondeo).py
+++ UD1/PYTHON/Ejercicio22 (Redondeo).py
@@ -1,3 +1,5 @@
 n = float(input("Introduce un número decimal: "))
-redondeado = round(n)
-print(f"El número {n} redondeado es {redondeado}")
+if n < 0:
+    n = abs(n) # Convertimos a positivo si es negativo con abs() que devuelve el valor absoluto de un numero
+    redondeado = round(n)
+    print(f"El número {n} redondeado es {redondeado}")
