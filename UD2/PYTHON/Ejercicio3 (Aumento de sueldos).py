c8568810691ecdede5c1f460e40c6020e9ce00d6
RaulPerezAguilar <rperagu1310@g.educaand.es>
2025-10-29 12:42:43 +0100

    Ejercicios UD2

--- UD2/PYTHON/Ejercicio3 (Aumento de sueldos).py
+++ UD2/PYTHON/Ejercicio3 (Aumento de sueldos).py
@@ -1,5 +1,6 @@
 sueldos = [1800, 1200, 2000, 1200, 900]
 aumento = 0.10
+print(sueldos)
 for i in range(len(sueldos)):
     sueldos[i] = sueldos[i] + sueldos[i] * aumento  # Aplicar aumento del 10%
 print(sueldos)
