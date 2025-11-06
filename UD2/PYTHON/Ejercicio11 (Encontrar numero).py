265f3a9c7523c89b4262e3ec9d1e60ae81e88ca6
rperagu1310 <rperagu1310@g.educaand.es>
2025-11-06 09:14:06 +0100

    Index

--- UD2/PYTHON/Ejercicio11 (Encontrar numero).py
+++ UD2/PYTHON/Ejercicio11 (Encontrar numero).py
@@ -1,14 +1,17 @@
+#Buscar un numero
+def buscar():
+    if n in numeros:
+        return numeros.index(n) #Si el número está en la lista, muestra su posición
+    else:
+        return "El número no se encuentra en la lista."
+
+
 numeros = [1,2,3,4,5,6,7,8,9,10]
 n = int(input("Introduce un número del 1 al 10: "))
-if n in numeros:
-    print("El numero esta en la posicon", numeros.index(n)) #Si el número está en la lista, muestra su posición
-else:
-    print("El número no se encuentra en la lista.")
+print("El numero esta en la posicion", buscar())
 
-numeros2 = [11,12,13,14,15,16,17,18,19,20]
-m = int(input("Añade un numero al array: "))
-numeros2.append(m)
-if m in numeros2:
-    print("El numero esta en la posicon", numeros2.index(m)) #Si el número está en la lista, muestra su posición
-else:
-    print("El número no se encuentra en la lista.")
+
+#Añadir un número al array y buscar su posición
+n = int(input("Añade un numero al array: "))
+numeros.append(n)
+print("El numero esta en la posicion", buscar())
