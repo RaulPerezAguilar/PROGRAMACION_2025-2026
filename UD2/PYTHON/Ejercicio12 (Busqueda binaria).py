8cf634069bddb3977562bc51520c9268f02d2ddf
rperagu1310 <rperagu1310@g.educaand.es>
2025-11-10 13:37:10 +0100

    Binario

--- UD2/PYTHON/Ejercicio12 (Busqueda binaria).py
+++ UD2/PYTHON/Ejercicio12 (Busqueda binaria).py
@@ -1,18 +1,18 @@
-Array = [0,10,20,30,40,50,60,70,80,90]
+array = [0,10,20,30,40,50,60,70,80,90]
 
-def busqueda_binaria(array, objetivo):
+def busqueda_binaria(array, n):
     i = 0
     j = len(array) - 1
     while i <= j:
         medio = (i + j) // 2 # Calcular el indice del elemento medio
-        if array[medio] == objetivo: # Si el elemento medio es el objetivo, devolver su indice
+        if array[medio] == n: # Si el elemento medio es el objetivo, devolver su indice
             return medio 
-        elif array[medio] < objetivo: # Si el objetivo es mayor, buscar en la mitad derecha
+        elif array[medio] < n: # Si el objetivo es mayor, buscar en la mitad derecha
             i = medio + 1
         else:
             j = medio - 1 # Si el objetivo es menor, buscar en la mitad izquierda
     return -1
 
-print("Array:", Array)
+print("Array:", array)
 n = int(input("Introduce un numero a buscar: "))
-print("El numero se encuentra en la posicion:", busqueda_binaria(Array, n))
+print("El numero se encuentra en la posicion:", busqueda_binaria(array, n))
