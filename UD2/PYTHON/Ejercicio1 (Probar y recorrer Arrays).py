95a965d5cba61851061a0757d6a8de42cd45d99f
rperagu1310 <rperagu1310@g.educaand.es>
2025-11-24 16:16:37 +0100

    a

--- UD2/PYTHON/Ejercicio1 (Probar y recorrer Arrays).py
+++ UD2/PYTHON/Ejercicio1 (Probar y recorrer Arrays).py
@@ -6,15 +6,12 @@ for i in range(len(Array)):
     suma += Array[i]
 print(suma)
 
-
-
 #for each
 suma2=0
 Array2 = [25,12,30,10,8]
 for i in Array2:
     suma2 += i
 print(suma2)
-
 
 #while
 suma3=0
@@ -25,6 +22,3 @@ while i < longitud:
     suma3 += Array3[i]
     i += 1
 print(suma3)
-
-
-  
