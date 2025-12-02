1147647deceb92f54ecd5a0b326cc442aa4eedc7
Raul Perez <rperagu1310@g.educaand.es>
2025-12-02 14:51:49 +0100

    UD3

---
+++ UD3/src/main/java/AntonioRecio/PescadoFresco/EJ2_Edad_Proxima.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package AntonioRecio.PescadoFresco;
+import java.util.Scanner;
+/**
+ *
+ * @author 01_1DAW_Alum
+ */
+public class EJ2_Edad_Proxima {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Introduce tu edad: ");
+        int edad = sc.nextInt();
+        int nueva_edad = ++edad;
+        System.out.print("El año que viene tendras: " + nueva_edad + " Años");
+        // TODO code application logic here
+    }
+    
+}
