1bf67c27df73ded146bfb1961ba5fec7548b0dde
Raul Perez <rperagu1310@g.educaand.es>
2025-12-11 09:07:07 +0100

    :3

---
+++ UD3/src/main/java/AntonioRecio/PescadoFresco/a.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package AntonioRecio.PescadoFresco;
+
+/**
+ *
+ * @author 01_1DAW_Alum
+ */
+public class a {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
