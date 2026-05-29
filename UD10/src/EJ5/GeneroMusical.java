7fdc4d8afa15955f3a20fa8b4c15b223311b77a0
rperagu1310 <rperagu1310@g.educaand.es>
2026-05-29 14:29:57 +0200

    EJ5

---
+++ UD10/src/EJ5/GeneroMusical.java
@@ -0,0 +1,17 @@
+package EJ5;
+
+/**
+ * Enum que define géneros musicales.
+ */
+
+public enum GeneroMusical {
+
+    ROCK,
+    POP,
+    REGGAETON,
+    JAZZ,
+    ELECTRONICA,
+    RAP,
+    FLAMENCO,
+    CLASICA
+}
